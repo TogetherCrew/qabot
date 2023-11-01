2d0a2d00b9a97493b2dc25e79992356e29a68966
TjitsevdM <48472085+TjitsevdM@users.noreply.github.com>
2023-11-01 13:47:43 -0700

    prompt update to use task_complete tool in right way

--- packages/ml/src/llm/reason/schema.py
+++ packages/ml/src/llm/reason/schema.py
@@ -5,9 +5,9 @@ class JsonSchema:
             "task": "Make a description of the question under [QUESTION] and the related task assigned to you under [TASK]",
             "knowledge": "If there is any helpful information related to task in [RELATED KNOWLEDGE], summarize the key points here in one sentence. Otherwise respond 'N/A'",
             "past_events": "If there is any helpful information related to task in [RELATED PAST EPISODES], summarize the key points here. Otherwise respond 'N/A'",
-            "reasoning": "Reasoning about the action to complete the task based on information in knowledge and past_events and using one of the tools in [TOOLS]. If the task can be completed, always use the task_complete tool. Don't ever answer questions based on prior knowledge only",
-            "idea": "Idea for an action using one of the tools in [TOOLS] to complete the task based on information in knowledge and past_events. If the task can be completed, always use the task_complete tool.",
-            "criticism": "If the task can be completed based on the previously gathered information summarized under knowledge and past_events, respond 'The task_complete tool must be used. This decision overrides reasoning and idea'. Otherwise respond 'N/A'",
+            "reasoning": "Reasoning about the action to complete the task based on information in knowledge and past_events and using one of the tools in [TOOLS]. If the task can be completed, always use the task_complete tool to provide an answer. Don't ever answer questions based on prior knowledge only",
+            "idea": "Idea for an action using one of the tools in [TOOLS] to complete the task based on information in knowledge and past_events. If the task can be completed, always use the task_complete tool to provide ann answer.",
+            "criticism": "If the task can be completed based on the previously gathered information summarized under knowledge and past_events, respond 'The task_complete tool must be used to provide an answer. This decision overrides reasoning and idea'. Otherwise respond 'N/A'",
             "summary": "Summary of the reasoning, action idea and criticism for the task based on knowledge and past_events",
         },
         "action": {
